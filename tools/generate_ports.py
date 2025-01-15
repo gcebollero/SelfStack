ce9d20150fb5bff7f6634e13910e22baa11908fb
gcebollero <guillermocebollero@gmail.com>
2025-01-15 11:49:35 +0100

    test workflow for port doc

---
+++ tools/generate_ports.py
@@ -0,0 +1,14 @@
+from glob import glob
+import yaml
+import pandas
+
+if __name__ == "__main__":
+    _results = []
+    for _file in glob('../stacks/*.yml'):
+        _f = yaml.safe_load(open(_file, 'r'))
+        for _service in _f.get('services', []):
+            for _ports in _f['services'][_service].get('ports', []):
+                _results.append({'Container': _service, 'Host port': _ports.split(':')[0], 'Container port': _ports.split(':')[1]})
+    df = pandas.DataFrame(_results)
+    with open('../ports.md', 'w') as md:
+        md.write(df.to_markdown())
